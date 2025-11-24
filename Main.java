61941f5d0e4a57cdfdb18ed338266b1b142a94ad
PardalV <145386771+PardalV@users.noreply.github.com>
2025-11-24 18:03:00 -0300

    Finalização da main

--- Main.java
+++ Main.java
@@ -35,9 +35,95 @@ public class Main
 		}
 	}
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		HeapMaxima meuHeap = new HeapMaxima(20);
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Sistema de cargas iniciando");
+		HeapMaxima meuHeap = new HeapMaxima(10);
+		Scanner sc = new Scanner(System.in);
+		int opcao = 0;
+
+		while(opcao != 6){
+			System.out.println("\n--- SISTEMA DE GERENCIAMENTO DE CARGAS ---");
+			System.out.println("1. Carregar do CSV");
+            System.out.println("2. Inserir Nova Carga Manualmente");
+            System.out.println("3. Consultar Carga de Maior Prioridade");
+            System.out.println("4. Remover Carga de Maior Prioridade");
+            System.out.println("5. Exibir Relatório Completo (Ordenado)");
+            System.out.println("6. Sair");
+            System.out.print("Escolha uma opção: ");
+
+			try{
+				String input = sc.nextLine();
+				opcao = Integer.parseInt(input);
+
+			} catch (NumberFormatException e){
+				System.out.println("Por favor, digite apenas números válidos");
+				continue;
+			}
+			switch(opcao){
+				case 1:
+					System.out.println("Digite o nome do arquivo (ex: dados.csv): ");
+					String nomeArq = sc.nextLine();
+					carregarDadosCSV(nomeArq, meuHeap);
+					break;
+				case 2:
+					System.out.println("--- Inserção Manual ---");
+					try{
+						System.out.print("ID: ");
+						int id = Integer.parseInt(sc.nextLine());
+						System.out.print("Tipo : ");
+						int tipo = Integer.parseInt(sc.nextLine());
+						System.out.print("Urgência: ");
+						int urgencia = Integer.parseInt(sc.nextLine());
+						System.out.print("Peso: ");
+						int peso = Integer.parseInt(sc.nextLine());
+						System.out.print("Descrição: ");
+						String descricao = sc.nextLine();
+
+						Carga nova = new Carga(id, tipo, urgencia, peso, descricao);
+						meuHeap.inserir(nova);
+						System.out.println("Carga inserida com prioridade: " + nova.getPrioridade());
+					} catch (NumberFormatException e){
+						System.out.println("Erro na digitação dos números. Tente novamente");
+					}
+					break;
+				case 3:
+					Carga topo = meuHeap.consultarTopo();
+					if(topo != null){
+						System.out.println("Topo atual: " + topo);
+					} else {
+						System.out.println("A Heap está vazia");
+					}
+					break;
+				case 4:
+					Carga removida = meuHeap.removerMaximo();
+					if(removida != null){
+						System.out.println("Carga removida e despachada: " + removida);
+					}
+					break;
+				case 5:
+					System.out.println("\n--- Relatório de Cargas (Ordenado por Prioridade) ---");
+					if (meuHeap.estaVazia()){
+						System.out.println("Nenhuma carga cadastrada");
+					} else {
+						Carga[] dadosCopia = meuHeap.getVetorCopia();
+						HeapMaxima auxHeap = new HeapMaxima(dadosCopia.length);
+						for(int i = 1; i < dadosCopia.length; i ++){
+							if(dadosCopia[i] != null){
+								auxHeap.inserir(dadosCopia[i]);
+							}
+						}
+						System.out.println("Prioridade | ID | Descrição");
+						System.out.println("-----------------------------------");
+						while(!auxHeap.estaVazia()){
+							System.out.println(auxHeap.removerMaximo());
+						}
+					}
+					break;
+				case 6:
+					System.out.println("Encerrando o sistema");
+					break;
+				default:
+					System.out.println("Opção inválida! Escolha um número válido");
+			}
+		}
+		sc.close();
 	}
 }
